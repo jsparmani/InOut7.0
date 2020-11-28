f19842fb77c16aa224ab58dbf0233d344fc5f9fe
parthsujalshah <parthsujalshah@gmail.com>
2020-11-28 22:50:48 +0530

    added product model

---
+++ server/src/entity/Product.ts
@@ -0,0 +1,22 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Product extends BaseEntity {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Field()
+    @Column()
+    name: string;
+
+    @Field()
+    @Column()
+    code: string;
+
+    @Field()
+    @Column()
+    availableQty: number;
+}
