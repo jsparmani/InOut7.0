f28c10f74978ba355503ee42493968eb98a004e7
parthsujalshah <parthsujalshah@gmail.com>
2020-11-29 12:55:10 +0530

    validateProfileCreate

--- server/src/resolvers/profile.ts
+++ server/src/resolvers/profile.ts
@@ -1,8 +1,33 @@
-import { Arg, Resolver } from "type-graphql";
+import { Profile } from "src/entity/Profile";
+import { FieldError } from "src/types/FieldError";
+import { MyContext } from "src/types/MyContext";
+import { validateProfileCreate } from "src/utils/validateProfile";
+import { Arg, Ctx, Field, ObjectType, Resolver } from "type-graphql";
+import { ProfileInput } from "./inputs/ProfileInput";
+
+@ObjectType()
+class ProfileResponse {
+    @Field(() => Profile, {nullable: true})
+    profile?: Profile;
+
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+}
 
 @Resolver()
 export class ProfileResolver {
     async createProfile(
-        @Arg("input")
-    )
+        @Arg("input", () => ProfileInput) input: ProfileInput,
+        @Ctx() {res}: MyContext
+    ): Promise<ProfileResponse> {
+        const {name, age, gender, phone} = input;
+
+        const errors = validateProfileCreate(input)
+
+        if(errors) {
+            return {errors};
+        }
+
+        
+    }
 }
---
+++ server/src/utils/validateProfile.ts
@@ -0,0 +1,9 @@
+import { ProfileInput } from "src/resolvers/inputs/ProfileInput";
+
+export const validateProfileCreate = (options: ProfileInput) => {
+    const {name, age, gender, phone} = options;
+    if(phone.length() !== 10) {
+        return [{field: "phone", message: "Invalid phone number"}];
+    }
+    return null;
+};
