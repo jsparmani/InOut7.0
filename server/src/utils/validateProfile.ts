be95cbaa3b4f135ea719c37f8055904f3bf09348
parthsujalshah <parthsujalshah@gmail.com>
2020-11-29 15:18:19 +0530

    string length call removed

--- server/src/utils/validateProfile.ts
+++ server/src/utils/validateProfile.ts
@@ -2,7 +2,7 @@ import { ProfileInput } from "src/resolvers/inputs/ProfileInput";
 
 export const validateProfileCreate = (options: ProfileInput) => {
     const {name, age, gender, phone} = options;
-    if(phone.length() !== 10) {
+    if(phone.length !== 10) {
         return [{field: "phone", message: "Invalid phone number"}];
     }
     return null;
