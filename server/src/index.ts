dc71a9add9e7381a3afb2e3fc015ccb4345f2c31
Kush Daga <kushdaga@hotmail.com>
2020-12-14 15:03:45 +0530

    Added packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,75 +1,80 @@
-import {verify} from "jsonwebtoken";
-import {UserResolver} from "./resolvers/user";
+import { verify } from "jsonwebtoken";
+import { UserResolver } from "./resolvers/user";
 import "reflect-metadata";
-import {ApolloServer} from "apollo-server-express";
-import {buildSchema} from "type-graphql";
-import {createConnection} from "typeorm";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {User} from "./entity/User";
-import {createRefreshToken, createAccessToken} from "./utils/auth";
-import {sendRefreshToken} from "./utils/sendRefreshToken";
+import { User } from "./entity/User";
+import { createRefreshToken, createAccessToken } from "./utils/auth";
+import { sendRefreshToken } from "./utils/sendRefreshToken";
 import { ProfileResolver } from "./resolvers/profile";
 import { AddressResolver } from "./resolvers/address";
 import { StoreResolver } from "./resolvers/store";
 
 (async () => {
-    const app = express();
-    app.use(
-        cors({
-            origin: "http://localhost:3000",
-            credentials: true,
-        })
-    );
+	const app = express();
+	app.use(
+		cors({
+			origin: "http://localhost:3000",
+			credentials: true,
+		})
+	);
 
-    app.use(cookieParser());
-    app.get("/", (_req, res) => res.send("hello"));
-    app.post("/refresh_token", async (req, res) => {
-        const token = req.cookies.jid;
-        if (!token) {
-            return res.send({ok: false, accessToken: ""});
-        }
-        let payload: any = null;
-        try {
-            payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
-        } catch (err) {
-            console.log(err);
-            return res.send({ok: false, accessToken: ""});
-        }
+	app.use(cookieParser());
+	app.get("/", (_req, res) => res.send("hello"));
+	app.post("/refresh_token", async (req, res) => {
+		const token = req.cookies.jid;
+		if (!token) {
+			return res.send({ ok: false, accessToken: "" });
+		}
+		let payload: any = null;
+		try {
+			payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+		} catch (err) {
+			console.log(err);
+			return res.send({ ok: false, accessToken: "" });
+		}
 
-        const user = await User.findOne({id: payload.userId});
+		const user = await User.findOne({ id: payload.userId });
 
-        if (!user) {
-            return res.send({ok: false, accessToken: ""});
-        }
+		if (!user) {
+			return res.send({ ok: false, accessToken: "" });
+		}
 
-        if (user.tokenVersion !== payload.tokenVersion) {
-            return res.send({ok: false, accessToken: ""});
-        }
+		if (user.tokenVersion !== payload.tokenVersion) {
+			return res.send({ ok: false, accessToken: "" });
+		}
 
-        sendRefreshToken(res, createRefreshToken(user));
+		sendRefreshToken(res, createRefreshToken(user));
 
-        return res.send({ok: true, accessToken: createAccessToken(user)});
-    });
+		return res.send({ ok: true, accessToken: createAccessToken(user) });
+	});
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [UserResolver, ProfileResolver, AddressResolver, StoreResolver],
-            validate: false,
-        }),
-        context: ({req, res}) => ({req, res}),
-    });
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [
+				UserResolver,
+				ProfileResolver,
+				AddressResolver,
+				StoreResolver,
+			],
+			validate: false,
+		}),
+		context: ({ req, res }) => ({ req, res }),
+	});
 
-    const conn = await createConnection();
+	const conn = await createConnection();
 
-    await conn.runMigrations();
+	await conn.runMigrations();
 
-    apolloServer.applyMiddleware({app});
+	apolloServer.applyMiddleware({ app, cors: false });
 
-    const port: any = process.env.PORT;
-    app.listen(port, () => {
-        console.log(`Express server started on ${port}`);
-    });
+	const port: any = process.env.PORT;
+	app.listen(port, () => {
+		console.log(`Express server started on ${port}`);
+	});
 })();
